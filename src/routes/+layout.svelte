f901c159cfae28d5d4041bf2dfa076489fa4a70e
Joni Ahola <joni.ahola@sohova.fi>
2022-12-30 01:12:08 +0200

    Initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,30 @@
+<script>
+	import './styles.css';
+	import Sidebar from './Sidebar.svelte';
+</script>
+
+<div class="app">
+	<Sidebar />
+	<main>
+		<slot />
+	</main>
+</div>
+
+<style>
+	.app {
+		display: flex;
+		flex-direction: column;
+		min-height: 100vh;
+	}
+
+	main {
+		flex: 1;
+		display: flex;
+		flex-direction: column;
+		padding: 1rem;
+		width: 100%;
+		max-width: 64rem;
+		margin: 0 auto;
+		box-sizing: border-box;
+	}
+</style>
---
+++ src/routes/settings/+page.svelte
@@ -0,0 +1,11 @@
+<section>
+	<h1>
+		Asetukset
+	</h1>
+
+    <form>
+        <input type="text" name="name">
+    </form>
+	
+    <a href="/">Takaisin</a>
+</section>
